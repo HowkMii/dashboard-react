ea8acd9f46bd511d3b449e271bcaa951c690c611
mostefa <21nounou21@gmail.com>
2021-09-30 20:25:32 +0100

    added animations

--- src/index.js
+++ src/index.js
@@ -16,3 +16,9 @@ var animations = hakim.getAnimations()
 for(var i =1; i<hakim.clientHeight;i++){
   animations.values[i]= animations.values[i+1]
 }
+var hakim = document.getElementById("this-element")
+var animations = hakim.getAnimations()
+
+for(var i =1; i<hakim.clientHeight;i++){
+  animations.values[i]= animations.values[i+1]
+}
